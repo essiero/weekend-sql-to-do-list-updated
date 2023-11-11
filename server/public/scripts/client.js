613ccd623f506d447e5e5e7eca53d32f2d385a89
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2023-11-11 17:55:02 -0600

    made delete function, request, route

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -12,14 +12,14 @@ X form for to-dos
             X render response.data to DOM
                 X clear list
                 X render database list to DOM
-    - complete button
-        - onclick attribute to preventDefault, completeTask()
-        - completeTask(event) function
+    X complete button
+        X onclick attribute to preventDefault, completeTask()
+        - markAsComplete(event) function
             - add completed class
             - edit CSS for .complete to strikethru
         - disable complete button when a to-do has been marked completed
-    - delete button
-        - onclick attribute to deleteTask()
+    X delete button
+        X onclick attribute to deleteTask()
         - deleteTask(event) function to remove task from database
             - delete request
             - delete route
@@ -43,7 +43,7 @@ X form for to-dos
 
 */
 console.log('JS is sourced!');
-getTodos();
+getToDos();
 
 // Get request
 function getToDos() {
@@ -51,18 +51,24 @@ function getToDos() {
         type: 'GET',
         url: '/todos'
     }).then((res) => {
-        renderTodos(res.data)
+        renderToDos(res.data)
     }).catch((error) => {
 console.log('Error in GET request', error)
     });
 };
 
+/* Renders to-do items to list table */
 function renderToDos(todos) {
     const taskList = document.getElementById('list');
     taskList.innerHTML = '';
     for (let item of todos){
-        taskList.innerHTML +=
-        `<li>${item.text}</li>`
+        taskList.innerHTML +=  ` 
+        <tr data-todoId="${item.id}">
+        <td>${item.text}</td>
+        <td><button onclick="markComplete(event)">Complete</button></td>
+        <td><button onclick="deleteToDo(event)">Delete item</button></td>
+        </tr>
+        `
     }
 }
 
@@ -72,7 +78,7 @@ function handleSubmit(event){
     let newTask = {};
     newTask.text = document.getElementById('toDoText').value;
     newTask.isComplete = false;
-// POST request for new to-do
+/* POST request for new to-do */
     axios({
         method: 'POST',
         url: '/list',
@@ -86,3 +92,30 @@ function handleSubmit(event){
     })
 }
 
+// markComplete(event) function here
+    // change css class to completed
+    // strikethru text
+    // disable complete button
+
+/* Deletes selected To-Do item */
+function deleteToDo(event){
+    let clickedButton = event.target;
+    let TableRow = clickedButton.closest('tr');
+    let toDoID = TableRow.getAttribute('data-todoId');
+    console.log('Task ID', toDoID)
+
+    axios({
+        method: 'DELETE',
+        url: `/todos/${toDoID}`
+    }).then((response) => {
+        getToDos();
+    }).catch((error) => {
+        console.log('DELETE /todos/:id fail', error)
+    })
+    Swal.fire({
+        title: "Deleted!",
+        text: "Your file has been deleted.",
+        icon: "success"
+      });
+    
+}
--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -4,7 +4,8 @@ const pool = require('../modules/pool');
 // Get route all to-dos
 router.get('/', (req, res) => {
     let queryText = 'SELECT * from "todos" ORDER BY "id";';
-    pool.query(queryText).then(result => {
+    pool
+    .query(queryText).then(result => {
         // Sends back results in an object
         res.send(result.rows);
     })
@@ -14,5 +15,25 @@ router.get('/', (req, res) => {
     })
 })
 
+// DELETE item
+router.delete('/:id', (req, res) => {
+    let idOfTask = req.params.id;
+    const sqlText = `
+    DELETE from "todos"
+    WHERE "id" = $1;
+    `
+    const sqlValues = [idOfTask];
+
+    pool
+    .query(sqlText, sqlValues)
+    .then((dbResult) => {
+        res.sendStatus(200)
+    })
+    .catch((dbError) => {
+        console.log('DELETE /todos/:id failed', dbError);
+        res.sendStatus(500)
+
+    })
+})
 
 module.exports = router;
