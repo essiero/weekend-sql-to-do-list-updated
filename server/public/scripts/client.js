5fbf178cb63cfeaea2c91a35dd9e046793bf36c5
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2023-11-11 14:27:35 -0600

    added task input field and submit button, made get route for to-do list, made renderTodos function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,14 +1,67 @@
-console.log('JS is sourced!');
+/* TO-DO
+- create database
+- connect server to database
+    - 
+- form for to-dos
+    - input fields for to-do
+        - post route
+        - post request
+            - add form input to database
+        - get route
+        - get request
+            - render response.data to DOM
+                - clear table
+                - render database table to DOM
+    - complete button
+        - onclick attribute to preventDefault, completeTask()
+        - completeTask(event) function
+            - add completed class
+            - edit CSS for .complete to strikethru
+        - disable complete button when a to-do has been marked completed
+    - delete button
+        - onclick attribute to deleteTask()
+        - deleteTask(event) function to remove task from database
+            - delete request
+            - delete route
+    
 
+    STRETCH GOALS
+        - click on words to edit 
+            - might just need to add contentEditable attribute to <td> tag??
+        - style like pad of paper with lines
+        - animate erase on delete
+
+
+
+
+
+
+
+
+
+
+
+*/
+console.log('JS is sourced!');
+getTodos();
+
+// Get request
 function getTodos() {
     axios({
         type: 'GET',
         url: '/todos'
-    }).then(res => {
+    }).then((res) => {
         renderTodos(res.data)
-    })
-}
+    }).catch((error) => {
+console.log('Error in GET request', error)
+    });
+};
 
 function renderTodos(todos) {
-    
+    const taskList = document.getElementById('list');
+    taskList.innerHTML = '';
+    for (let item of todos){
+        taskList.innerHTML +=
+        `<li>${item.text}</li>`
+    }
 }
--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
 
-
+// Get route all to-dos
+router.get('/', (req, res) => {
+    let queryText = 'SELECT * from "todos" ORDER BY "id";';
+    pool.query(queryText).then(result => {
+        // Sends back results in an object
+        res.send(result.rows);
+    })
+    .catch(error => {
+        console.log('Error retrieving tasks', error);
+        res.sendStatus(500);
+    })
+})
 
 
 module.exports = router;
